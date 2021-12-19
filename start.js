9c4fa82f0775633010df7a41497ef50107dc1996
Tearat <raiiliiat@yandex.ru>
2021-12-19 17:59:34 +0500

    Fix default port

--- start.js
+++ start.js
@@ -1,22 +1,25 @@
-const express = require('express')
-const routes = require('./src/routes/routes')
-const bodyParser = require('body-parser')
-const path = require('path')
+const express = require("express")
+const routes = require("./src/routes/routes")
+const bodyParser = require("body-parser")
+const path = require("path")
 
-const envRealPath = path.resolve(__dirname, '.env')
-require('dotenv').config( {path: envRealPath} )
+const envRealPath = path.resolve(__dirname, ".env")
+require("dotenv").config({ path: envRealPath })
 
 const app = express()
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
-    res.header('Access-Control-Allow-Headers', 'Content-Type')
-    next();
-});
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
+  res.header("Access-Control-Allow-Headers", "Content-Type")
+  next()
+})
 
 app.use(routes)
-app.use('/storage', express.static(__dirname + "/storage/"));
+app.use("/storage", express.static(__dirname + "/storage/"))
 
-app.listen(process.env.PORT, () => console.log(`>>> Server starts (port ${process.env.PORT})`))
-    .on('error', error => console.warn(error))
+const port = process.env.PORT || 8000
+
+app
+  .listen(port, () => console.log(`>>> Server starts (port ${port})`))
+  .on("error", (error) => console.warn(error))
