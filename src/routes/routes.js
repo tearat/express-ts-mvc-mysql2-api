07ce8ebdd5a6e4fd98c559258751d9b7b820768f
Tearat <raiiliiat@yandex.ru>
2021-12-19 20:19:52 +0500

    app & router refactor

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,21 +1,21 @@
-const express = require('express')
+const express = require("express")
 const router = express.Router()
-const booksController = require('../controllers/booksController')
-const bodyParser = require('body-parser')
-const fileParser = require('express-multipart-file-parser')
+const booksController = require("../controllers/booksController")
 
-router.use(bodyParser.urlencoded({
-    extended: false
-}));
-router.use(bodyParser.json())
-router.use(fileParser)
+router.get("/api", (req, res) => res.send({ status: "API ready" }))
+router.get("/api/books", async (req, res) => booksController.all(req, res))
+router.get("/api/books/:id", async (req, res) => booksController.find(req, res))
+router.get("/api/books/isbn/:isbn", async (req, res) =>
+  booksController.findByIsbn(req, res)
+)
+router.post("/api/books", async (req, res) =>
+  booksController.create(req, res, req.body, req.files[0])
+)
+router.put("/api/books/:id", async (req, res) =>
+  booksController.update(req, res, req.body, req.files[0])
+)
+router.delete("/api/books/:id", async (req, res) =>
+  booksController.delete(req, res)
+)
 
-router.get('/api', (req, res) => res.send({ status: "API ready" }))
-router.get('/api/books', async (req, res) => booksController.all(req, res))
-router.get('/api/books/:id', async (req, res) => booksController.find(req, res))
-router.get('/api/books/isbn/:isbn', async (req, res) => booksController.findByIsbn(req, res))
-router.post('/api/books', async (req, res) => booksController.create(req, res, req.body, req.files[0]))
-router.put('/api/books/:id', async (req, res) => booksController.update(req, res, req.body, req.files[0]))
-router.delete('/api/books/:id', async (req, res) => booksController.delete(req, res))
-
-module.exports = router;
+module.exports = router
--- start.js
+++ start.js
@@ -1,7 +1,8 @@
 const express = require("express")
 const routes = require("./src/routes/routes")
-const bodyParser = require("body-parser")
 const path = require("path")
+const bodyParser = require("body-parser")
+const fileParser = require("express-multipart-file-parser")
 
 const envRealPath = path.resolve(__dirname, ".env")
 require("dotenv").config({ path: envRealPath })
@@ -14,6 +15,14 @@ app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Headers", "Content-Type")
   next()
 })
+
+app.use(
+  bodyParser.urlencoded({
+    extended: false,
+  })
+)
+app.use(bodyParser.json())
+app.use(fileParser)
 
 app.use(routes)
 app.use("/storage", express.static(__dirname + "/storage/"))
